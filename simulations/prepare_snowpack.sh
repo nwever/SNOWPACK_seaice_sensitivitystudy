0ddd6752fb29b280fcc3b076ff729a165aeaf549
Nander Wever <nander.wever@slf.ch>
2023-09-18 12:31:50 +0200

    Adding flexible domain initialization

--- simulations/prepare_snowpack.sh
+++ simulations/prepare_snowpack.sh
@@ -4,12 +4,13 @@ setup_model () {
 	#
 	# Create sno file
 	#
-	python3 create_snofile.py -stn mosseb_level2v3seaice_CO -lat 85.618 -lon 125.832 -nodata -999 -date 2019-10-31T00:00 -s1 0 -s2 40 -s3 60 -s4 90 > ./input/C0_${experiment}.sno
+	domain=($(echo ${ice_thickness_below_sealevel} ${ice_thickness_above_sealevel} ${snow_thickness} | mawk -v L=${layer_thickness} '{print 0, $3/L, ($2+$3)/L, ($1+$2+$3)/L}'))
+	python3 create_snofile.py -stn mosseb_level2v3seaice_CO -lat ${lat} -lon ${lon} -nodata ${nodata} -date 2019-10-31T00:00 -s1 ${domain[0]} -s2 ${domain[1]} -s3 ${domain[2]} -s4 ${domain[3]} > ./input/C0_${experiment}.sno
 
 	#
 	# Create ini file
 	#
-	echo "IMPORT_BEFORE		=	./io_base.ini" > ${inifile}
+	echo "IMPORT_BEFORE	=	./io_base.ini" > ${inifile}
 	echo "" >> ${inifile}
 	echo "[INPUT]" >> ${inifile}
 	echo "STATION1		=	seaice_CO1.smet" >> ${inifile}
@@ -24,6 +25,19 @@ setup_model () {
 	> to_exec.lst
 	echo "snowpack -b 2019-11-01T00:00 -c ${inifile} -e NOW" >> to_exec.lst
 }
+
+
+# Location settings
+lat=85.618
+lon=125.832
+nodata=-999
+
+
+# Default settings
+ice_thickness_below_sealevel=60		# in cm
+ice_thickness_above_sealevel=20		# in cm
+snow_thickness=20			# in cm
+layer_thickness=2.0			# in cm, 2 cm is the assumption in create_snofile.py
 
 
 # Default experiment
